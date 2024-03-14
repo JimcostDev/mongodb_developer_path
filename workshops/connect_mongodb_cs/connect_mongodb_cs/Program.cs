098d947b0cca14d8d9d1847507e5e8fe5ee59907
JimcostDev <rjak727@gmail.com>
2024-03-14 17:32:55 +0100

    operaciones crud en c#

--- workshops/connect_mongodb_cs/connect_mongodb_cs/Program.cs
+++ workshops/connect_mongodb_cs/connect_mongodb_cs/Program.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 var mongodbUri = "mongodb://localhost:27017/";
 
@@ -10,4 +11,36 @@ Console.WriteLine("La lista de bases de datos en el servidor es:");
 foreach (var db in dbList)
 {
     Console.WriteLine(db);
+}
+
+
+// CRUD
+var database = client.GetDatabase("api_menu_db");
+var categoriesCollection = database.GetCollection<BsonDocument>("categories");
+
+// insertar documento
+var document = new BsonDocument
+{
+    {"name", "Hamburguesas" },
+    {"image","url_de_la_imagen" }
+};
+categoriesCollection.InsertOne(document);
+
+
+// actualizar un documento
+var filter = Builders<BsonDocument>.Filter.Eq("name", "Hamburguesas");
+var update = Builders<BsonDocument>.Update.Set("name", "PASTA");
+
+var result = categoriesCollection.UpdateOne(filter, update);
+Console.WriteLine(result.IsAcknowledged);
+
+// eliminar un documento
+var filterToDelete = Builders<BsonDocument>.Filter.Eq("name", "PASTA");
+categoriesCollection.DeleteOne(filterToDelete);
+
+// buscar documentos
+var categories = categoriesCollection.Find(_ => true).ToList();
+foreach (var category in categories)
+{
+    Console.WriteLine(category.ToString());
 }
