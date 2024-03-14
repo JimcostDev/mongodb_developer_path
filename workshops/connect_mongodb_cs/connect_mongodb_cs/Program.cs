598b7dbcaa9725bd83b9c7341925ec30be4438b1
JimcostDev <rjak727@gmail.com>
2024-03-14 16:41:58 +0100

    conectar mongodb con c#

---
+++ workshops/connect_mongodb_cs/connect_mongodb_cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+var mongodbUri = "mongodb://localhost:27017/";
+
+var client = new MongoClient(mongodbUri);
+
+var dbList = client.ListDatabaseNames().ToList();
+
+Console.WriteLine("La lista de bases de datos en el servidor es:");
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}
